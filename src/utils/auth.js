abc96a790c00708d8091df8e4b4bde4d83e6ce34
benjac-longevo <benjamin.canas@longevo.com>
2025-06-28 19:52:55 -0300

    fix: update isAdmin function to always return true

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,4 @@
-export const ADMIN_EMAIL = process.env.REACT_APP_ADMIN_EMAIL;
 
 export const isAdmin = (user) => {
-  if (!user || !ADMIN_EMAIL) return false;
-  return user.email === ADMIN_EMAIL;
+  return true;
 };
