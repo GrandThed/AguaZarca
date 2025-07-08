94469ea187bb023c48c59f22ee123568e0714d5b
GrandThed <32280623+GrandThed@users.noreply.github.com>
2025-07-08 16:46:13 -0300

    feat(blog): add cover image and formatting

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,22 @@
-const prefix = ""
+const prefix = "";
 
-export const HOME = prefix + "/"
-export const VENTA = prefix + "/venta"
-export const ALQUILER_TEMPORAL = prefix + "/alquiler-temporario"
-export const ALQUILER_ANUAL = prefix + "/alquiler-anual"
-export const NOTICIAS = prefix + "/noticias"
-export const CONTACTO = prefix + "/contacto"
-export const PUBLICAR = prefix + "/publicar-propiedad"
-export const PROPIEDAD = prefix + "/producto/"
-export const PRODUCTO = prefix + "/producto/:id"  //reminder: change the name of the routes
-export const TIPO_DE_PROPIEDAD_SIMPLE_URL = prefix + "/tipo/"
-export const TIPO_DE_PROPIEDAD = prefix + "/tipo/:type"
-export const BUSQUEDA_GLOBAL = prefix + "/busqueda"
-export const BLOG_CREATE = prefix + "/crear-blog"
+export const HOME = prefix + "/";
+export const VENTA = prefix + "/venta";
+export const ALQUILER_TEMPORAL = prefix + "/alquiler-temporario";
+export const ALQUILER_ANUAL = prefix + "/alquiler-anual";
+export const NOTICIAS = prefix + "/noticias";
+export const CONTACTO = prefix + "/contacto";
+export const PUBLICAR = prefix + "/publicar-propiedad";
+export const PROPIEDAD = prefix + "/producto/";
+export const PRODUCTO = prefix + "/producto/:id"; //reminder: change the name of the routes
+export const TIPO_DE_PROPIEDAD_SIMPLE_URL = prefix + "/tipo/";
+export const TIPO_DE_PROPIEDAD = prefix + "/tipo/:type";
+export const BUSQUEDA_GLOBAL = prefix + "/busqueda";
+export const BLOG_CREATE = prefix + "/crear-blog";
 
-export const EDITAR_PROPIEDAD = prefix + "/editar-propiedad/:id"
-export const DASHBOARD = prefix + "/dashboard/"
-export const REGISTRO = prefix + process.env.REACT_APP_SUPERSECRETEREGISTERROUTE
+export const BLOG_VIEW = prefix + "/blog/:id";
 
-export const BLOGS = prefix + "/blog"
-
- 
+export const EDITAR_PROPIEDAD = prefix + "/editar-propiedad/:id";
+export const DASHBOARD = prefix + "/dashboard/";
+export const REGISTRO = prefix + process.env.REACT_APP_SUPERSECRETEREGISTERROUTE;
+export const BLOGS = prefix + "/blog";
